e3f432693714bc26d65a877cf262fb6e23fad891
Blake Longfield <blakelongfield@gmail.com>
2018-10-12 16:54:58 -0600

    updated impl

--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -24,9 +24,40 @@ public class FilmDAOImpl implements FilmDAO {
 
 	@Override
 	public Film getFilmById(int filmId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Film film = null;
+
+		String sql = "SELECT film.id, film.title, film.description, film.release_year, language_id, language.name, + film.rating "
+				+ "FROM film " + "JOIN language " + "ON language.id = film.language_id " + "WHERE film.id = ?";
+
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+		ResultSet filmResult = stmt.executeQuery();
+		int counter = 0;
+
+		if (filmResult.next()) {
+			film = new Film();
+			film.setId(filmResult.getInt("id"));
+			film.setTitle(filmResult.getString("title"));
+			film.setDescription(filmResult.getString("description"));
+			film.setRelease_year(filmResult.getInt("release_year"));
+			film.setLanguage_id(filmResult.getInt("language_id"));
+			film.setLanguage_name(filmResult.getString("language.name"));
+			film.setRating(filmResult.getString("rating"));
+			film.setActors(getActorsByFilmId(filmId));
+			counter++;
+		}
+
+		if (film == null) {
+			System.out.println("\nWe apologize, but there is no title with that ID in our library.");
+		}
+
+		filmResult.close();
+		stmt.close();
+		conn.close();
+		System.out.println("\nYour search returned " + (counter) + " result.");
+		return film;
+	}		
 
 	@Override
 	public Film addFilm(Film newFilm) {
@@ -120,15 +151,59 @@ public class FilmDAOImpl implements FilmDAO {
 
 	@Override
 	public Actor getActorById(int actorId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Actor actor = null;
+		String sql = "SELECT id, first_name, last_name " + "FROM actor " + "WHERE id = ?";
 
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, actorId);
+		ResultSet actorResult = stmt.executeQuery();
+
+		if (actorResult.next()) {
+			actor = new Actor();
+			actor.setId(actorResult.getInt("id"));
+			actor.setFirst_name(actorResult.getString("first_name"));
+			actor.setLast_name(actorResult.getString("last_name"));
+		}
+
+		actorResult.close();
+		stmt.close();
+		conn.close();
+		return actor;
+
+	}	
+	
 	@Override
 	public List<Actor> getActorsByFilmId(int filmId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+			List<Actor> actorsByFilm = new ArrayList<>();
+
+			try {
+				Connection conn = DriverManager.getConnection(URL, user, pass);
+				String sql = "SELECT actor.id, actor.first_name, actor.last_name, " + "film_actor.film_id " + "FROM actor "
+						+ "JOIN film_actor " + "ON film_actor.actor_id = actor.id " + "WHERE film_id = ?";
+
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, filmId);
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					int actorId = rs.getInt("id");
+					String firstName = rs.getString("first_name");
+					String lastName = rs.getString("last_name");
+					Actor actor = new Actor(actorId, firstName, lastName);
+					actorsByFilm.add(actor);
+				}
+
+				rs.close();
+				stmt.close();
+				conn.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			return actorsByFilm;
+		}	
 
 	@Override
 	public List<Film> getFilmsByActorId(int actorId) throws SQLException {
