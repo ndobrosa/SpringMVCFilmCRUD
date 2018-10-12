f3cf54c32ca96d13519daac2008bb2b67fa61997
Joshua Ciccone-Okodo <joshokodo@gmail.com>
2018-10-12 15:25:10 -0600

    updated DAO interface

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -3,10 +3,19 @@ package com.skilldistillery.film.data;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
 	public Film getFilmById(int filmId) throws SQLException;
+
+	public List<Film> getFilmByKeyword(String keyword) throws SQLException;
+
+	public Actor getActorById(int actorId) throws SQLException;
+
+	public List<Actor> getActorsByFilmId(int filmId) throws SQLException;
+
+	public List<Film> getFilmsByActorId(int actorId) throws SQLException;
+
 	public Film addFilm(Film film);
-	public List<Film> getFilmsByKeyword(String keyword) throws SQLException;
 }
