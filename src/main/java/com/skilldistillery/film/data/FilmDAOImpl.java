9b8e9359bba5dac7371cfb5eb7759743f98aedc0
ndobrosa <ndobrosa@gmail.com>
2018-10-12 15:07:08 -0600

    added FilmDAOImpl class

---
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -0,0 +1,28 @@
+package com.skilldistillery.film.data;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.film.entities.Film;
+
+public class FilmDAOImpl implements FilmDAO {
+
+	@Override
+	public Film getFilmById(int filmId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film addFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> getFilmsByKeyword(String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
