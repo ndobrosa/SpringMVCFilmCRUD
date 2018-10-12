fe2569aad304277bc814f739bbf060cb5e890308
ndobrosa <ndobrosa@gmail.com>
2018-10-12 15:57:30 -0600

    Adjusted DAOImpl

--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -3,6 +3,7 @@ package com.skilldistillery.film.data;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
 public class FilmDAOImpl implements FilmDAO {
@@ -20,7 +21,25 @@ public class FilmDAOImpl implements FilmDAO {
 	}
 
 	@Override
-	public List<Film> getFilmsByKeyword(String keyword) throws SQLException {
+	public List<Film> getFilmByKeyword(String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor getActorById(int actorId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> getActorsByFilmId(int filmId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> getFilmsByActorId(int actorId) throws SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
